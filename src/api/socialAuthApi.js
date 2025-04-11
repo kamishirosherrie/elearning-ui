92a2385fd34f9299d80f6499b31f1051b97e077d
kamishirosherrie <sherrysjuvip@gmail.com>
2025-04-11 14:46:17 +0700

    fix facebook login

--- src/api/socialAuthApi.js
+++ src/api/socialAuthApi.js
@@ -1,5 +1,6 @@
 import axiosInstance from './axiosInstance'
 import { apiUrl } from './apiConfig'
+import axios from 'axios'
 
 export const getGoogleUserInfo = async (accessToken) => {
     try {
@@ -16,7 +17,7 @@ export const getGoogleUserInfo = async (accessToken) => {
 
 export const getFacebookUserInfo = async (accessToken) => {
     try {
-        const response = await axiosInstance.get(apiUrl.facebookUrl.getUserInfo(accessToken))
+        const response = await axios.get(apiUrl.facebookUrl.getUserInfo(accessToken))
         return response.data
     } catch (error) {
         console.error('Error fetching user info from Facebook: ', error.message)
