e77989a9550b1bce57acd78d47b93ea0a25125ab
kamishirosherrie <sherrysjuvip@gmail.com>
2025-04-16 10:22:42 +0700

    style sidebar with position fixed

--- src/layouts/MainLayout/MainLayout.js
+++ src/layouts/MainLayout/MainLayout.js
@@ -4,6 +4,7 @@ import styles from './MainLayout.module.scss'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
 import ScrollToTopButton from '../../components/ScrollToTopButton/ScrollToTopButton'
+import ScrollToTop from '../../components/ScrollToTop/ScrollToTop'
 
 const cx = classNames.bind(styles)
 
@@ -13,6 +14,7 @@ function MainLayout({ children }) {
             <Header />
             <div className={cx('children')}>{children}</div>
             <Footer />
+            <ScrollToTop />
             <ScrollToTopButton />
         </div>
     )
